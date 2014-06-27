3c11859968d9f30a973d6cb132a4e8023ac46b1c
rojasgk182 <ninahernandez@guillermos-mbp.wireless.dartmouth.edu>
2014-06-27 12:58:26 -0400

    Add Current CleryDart Code
    Current CleryDart HTML, JS, and CSS files

---
+++ cleryAppLocationVariables.js
@@ -0,0 +1,136 @@
+					//NEW LAT&LONG FOR DARTMOUTH BUILDINGS
+				//	var dicksHouse1 = new L.LatLng(43.70877,-72.28690);
+				//	var dicksHouse2 = new L.LatLng(43.70873 ,-72.28694);
+
+				//	var fiftyThreeCommons1 = new L.LatLng(43.70302, -72.29067);
+				//	var fiftyThreeCommons2 = new L.LatLng(43.70325, -72.29064);
+
+				//	var alphaChi1 = new L.LatLng(43.70698,-72.29281);
+				//	var alphaChi2 = new L.LatLng(43.70705 ,-72.29277);
+
+				//	var alphaDelta1 = new L.LatLng(43.70367 ,-72.28478 );
+				//	var alphaDelta2 = new L.LatLng(43.70363 ,-72.28474 );
+				//	var alphaDelta3 = new L.LatLng(43.70358 ,-72.28471 );
+
+				//	var alumniGym = new L.LatLng(43.70275 ,-72.28397 );
+				//	var amarnaSociety = new L.LatLng(43.70448 ,-72.28121 );
+				//	var aquinasHouse = new L.LatLng(43.70758 ,-72.29406 );
+				//	var bakerBerry = new L.LatLng(43.70534 ,-72.28867 );
+				//	var berryHall = new L.LatLng(43.70704 ,-72.28626 );
+
+				//	var betaFrat1 = new L.LatLng(43.70602 ,-72.29090 );
+				//	var betaFrat2 = new L.LatLng(43.70601 ,-72.29084 );
+				//	var betaFrat3 = new L.LatLng(43.70605 ,-72.29102 );
+
+				//	var bildnerHall = new L.LatLng(43.70721 ,-72.28600 );
+				//	var bonesGate = new L.LatLng(43.70621 ,-72.29184 );
+				//	var bossTennisCenter = new L.LatLng(43.70102 ,-72.27908 );
+				//	var braceCommons = new L.LatLng(43.70426 ,-72.28392 );
+
+				//	var brownHall1 = new L.LatLng(43.70731 ,-72.29031 );
+				//	var brownHall2 = new L.LatLng(43.70726,-72.29040 );
+
+				//	var carpenterHall = new L.LatLng(43.70551 ,-72.28925 );
+				//	var carsonHall = new L.LatLng(43.70583 ,-72.28910 );
+				//	var channingCox = new L.LatLng(43.70363 ,-72.29588 );
+				//	var cohenHall = new L.LatLng(43.70764 ,-72.29146 );
+
+				//	var collisCenter1 = new L.LatLng(43.70270 ,-72.28976 );
+				//	var collisCenter2 = new L.LatLng(43.70268 ,-72.28991 );
+				//	var collisCenter3 = new L.LatLng(43.70279 ,-72.28987 );
+				//	var collisCenter4 = new L.LatLng(43.70277 ,-72.29002 );
+				//	var collisCenter5 = new L.LatLng(43.70285 ,-72.29003 );
+				//	var collisCenter6 = new L.LatLng(43.70275,-72.28990);
+
+				//	var deweyLot = new L.LatLng(43.70887 ,-72.28299 );
+				//	var dicksHouseAdmin = new L.LatLng(43.70887 ,-72.28681 );
+				//	var dowdCountryInn1 = new L.LatLng( , ); //add coordinates
+				//	var dowdCountryInn2 = new L.LatLng( , ); //add coordinates
+				//	var faheyHall = new L.LatLng(43.70539 ,-72.29151 );
+
+				//	var frenchHall1 = new L.LatLng(43.70402, -72.29694);
+				//	var frenchHall2 = new L.LatLng(43.70398 , -72.29706);
+
+				//	var gammaDeltaChi = new L.LatLng(43.70658, -72.28995);
+
+				//	var gileHall = new L.LatLng(43.70475 ,-72.29121 );
+
+				//	var hanoverInn1 = new L.LatLng(43.70221 ,-72.28869 );
+				//	var hanoverInn2 = new L.LatLng(43.70216 ,-72.28892 );
+
+				//	var hanoverPd1 = new L.LatLng(43.70114 ,-72.28985 ); //Note:HPD is actually the location of the town hall
+				//	var hanoverPd2 = new L.LatLng(43.70114 ,-72.28977 );
+				//	var hanoverPd3 = new L.LatLng(43.70114 ,-72.28990 );
+				//	var hanoverPd4 = new L.LatLng(43.70113,-72.28975);
+
+				//	var hitchcockHall1 = new L.LatLng(43.70469 ,-72.29061 );
+				//	var hitchcockHall2 = new L.LatLng(43.70477 ,-72.29067 );
+
+				//	var hoodMuseum = new L.LatLng(43.70201 ,-72.28732 );
+				//	var hopkinsCenter = new L.LatLng(43.70192 ,-72.28810 );
+
+				//	var triKap1 = new L.LatLng(43.70629, -72.28987);
+				//	var triKap2 = new L.LatLng(43.70631, -72.28994);
+				//	var triKap3 = new L.LatLng(43.70633, -72.29003);
+
+				//	var lalacsHouse = new L.LatLng(43.70780 , -72.28879);
+				//	var mcullochHall = new L.LatLng(43.70459 ,-72.28335 );
+
+				//	var mclaneHall1 = new L.LatLng(43.70552 ,-72.29179 );
+				//	var mclaneHall2 = new L.LatLng(43.70556 ,-72.29192);
+				//	var mclaneHall3 = new L.LatLng(43.70562 ,-72.29207 );
+				//	var mclaneHall4 = new L.LatLng(43.70557,-72.29195 );
+
+				//	var midFayerweatherHall = new L.LatLng(43.70374 , -72.28639 );
+				//	var mortonHall = new L.LatLng(43.70405 ,-72.28356 );
+				//	var newHampshireHall = new L.LatLng(43.70245 ,-72.28675 );
+				//	var northFayerweatherHall = new L.LatLng(43.70406 ,-72.28635 );
+
+				//	var novackCafe1 = new L.LatLng(43.70577, -72.28838);
+				//	var novackCafe2 = new L.LatLng(43.70582 ,-72.28863 );
+
+				//	var twentyWestWheelock = new L.LatLng( , );  //add
+				//	var psiUpsilon = new L.LatLng(43.70272 ,-72.29062 );
+
+				//	var raunerResidenceHall1 = new L.LatLng(43.70749 , -72.28588);
+				//	var raunerResidenceHall2 = new L.LatLng(43.70739 , -72.28575);
+
+				//	var raunerLibrary = new L.LatLng(43.70447 ,-72.28817 );
+				//	var ripleyHall = new L.LatLng(43.70408 ,-72.28572 );
+				//	var robinsonHall = new L.LatLng(43.70311 ,-72.28988 );
+
+				//	var russellSage1 = new L.LatLng(43.70551 ,-72.29062 );
+				//	var russellSage2 = new L.LatLng(43.70543 ,-72.29064 );
+				//	var russellSage3 = new L.LatLng(43.70543, -72.29076 );
+				//	var russellSage4 = new L.LatLng(43.70542,-72.29067 );
+
+				//	var sanbornLibrary = new L.LatLng(43.70472 ,-72.28922);
+
+				//	var sigmaAlphaEpsilon1 = new L.LatLng(43.70584 ,-72.28782 );
+				//	var sigmaAlphaEpsilon2 = new L.LatLng(43.70583 ,-72.28771 );
+				//	var sigmaAlphaEpsilon3 = new L.LatLng(43.70584,-72.28769);
+				//	var sigmaAlphaEpsilon4 = new L.LatLng(43.70582,-72.28782);
+
+				//	var tabard1 = new L.LatLng(43.70645 ,-72.29058 );
+				//	var tabard2 = new L.LatLng(43.70650 ,-72.29067 );
+				//	var tabard3 = new L.LatLng(43.70643,-72.29056);
+
+				//	var thayerEngineering = new L.LatLng(43.70454 ,-72.29485 );
+				//	var thompsonArena = new L.LatLng (43.70106 ,-72.28023 );
+
+				//	var topliffHall1 = new L.LatLng (43.70269 ,-72.28594 );
+				//	var topliffHall2 = new L.LatLng (43.70268 ,-72.28617 );
+
+				//	var tuckHall = new L.LatLng(43.70563, -72.29442);
+
+				//	var wheelerHall1 = new L.LatLng (43.70508 ,-72.28720 );
+				//	var wheelerHall2 = new L.LatLng (43.70512 ,-72.28738 );
+
+				//	var zetaPsi1 = new L.LatLng (43.70610 ,-72.29148 );
+				//	var zetaPsi2 = new L.LatLng (43.70610 ,-72.29143 );
+				//	var zetaPsi3 = new L.LatLng (43.70604 ,-72.29161 );
+				//	var zetaPsi4 = new L.LatLng (43.70607 ,-72.29156 );
+				//	var zetaPsi5 = new L.LatLng (43.70603 ,-72.29147 );
+				//	var zetaPsi6 = new L.LatLng (43.70609 ,-72.29161 );
+				//	var zetaPsi7 = new L.LatLng (43.70603 ,-72.29158 );
+
